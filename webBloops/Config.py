6c7876ab3768b84bc637dd2badc6d5ded5397e2b
JD Cantrell <jdcantrell@gmail.com>
2011-10-27 17:48:40 -0700

    Add a basic config class, copy music to web root

---
+++ webBloops/Config.py
@@ -0,0 +1,13 @@
+'''Eventually this might actually load from a config file'''
+class Config:
+  def __init__(self):
+    self.WEB_PATH = '/'
+    self.WEB_ROOT = '/Users/john/Projects/output'
+    self.BASE_FOLDER = '/Users/john/webBloops'
+    self.TEMPLATE_PATH = '/Users/john/Projects/webBloops/templates'
+
+    #other config for work machine
+    self.BASE_FOLDER = '/Users/jcantrell/Downloads/webBloops'
+    self.WEB_ROOT = '/Users/jcantrell/Downloads/output'
+    self.TEMPLATE_PATH = '/Users/jcantrell/Projects/webBloops/templates'
+
--- webBloops/Parse.py
+++ webBloops/Parse.py
@@ -1,17 +1,19 @@
 '''This is the collection of functions needed to generate the site'''
 from jinja2 import Template, Environment, FileSystemLoader
 import MusicFolder
+import Config
 import os
+import shutil
 
-WEB_PATH = '/'
-WEB_ROOT = '/Users/john/Projects/output'
-BASE_FOLDER = '/Users/john/webBloops'
-TEMPLATE_PATH = '/Users/john/Projects/webBloops/templates'
+config = Config.Config()
 
-env = Environment(loader=FileSystemLoader(TEMPLATE_PATH))
+env = Environment(loader=FileSystemLoader(config.TEMPLATE_PATH))
 
 def create_file_pages(path, musicFolder):
-  web_folder = path.replace(BASE_FOLDER, WEB_ROOT)
+  web_folder = path.replace(config.BASE_FOLDER, config.WEB_ROOT)
+
+  if web_folder[len(web_folder) - 1] != '/':
+    web_folder += '/'
 
   try:
     os.makedirs(web_folder)
@@ -26,15 +28,23 @@ def create_file_pages(path, musicFolder):
   html = template.render(files=musicFolder.children)
   f.write(html)
 
-  #copy images
 
-  #copy music
+  #this assumes that all files are in leaf folders
+  #I'm okay with this for now
+  if len(musicFolder.children) == 0:
+    #copy images
+    for f in musicFolder.files['images']:
+      shutil.copy(config.BASE_FOLDER + f, config.WEB_ROOT + f)
+
+    #copy music
+    for f in musicFolder.files['music']:
+      shutil.copy(config.BASE_FOLDER + f, config.WEB_ROOT + f)
   
   #create children folders
   for i in musicFolder.children:
     create_file_pages(i.path, i)
       
 if __name__ == "__main__":
-  musicFolder = MusicFolder.MusicFolder(BASE_FOLDER, WEB_PATH)
-  create_file_pages(BASE_FOLDER, musicFolder)
+  musicFolder = MusicFolder.MusicFolder(config.BASE_FOLDER, config.WEB_PATH)
+  create_file_pages(config.BASE_FOLDER, musicFolder)
 
