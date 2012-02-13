12d0bc4c45fd41d68235b75c68069345ad84be1e
JD Cantrell <jdcantrell@gmail.com>
2012-02-12 19:48:56 -0800

    Update MusicFolder to use the new DirectoryList class

--- webBloops/MusicFolder.py
+++ webBloops/MusicFolder.py
@@ -1,51 +1,47 @@
-'''MusicFolder will recurse through the given directory and collect information
-about each level. It collects the images and available music files in a simple
-object for parsing'''
+from DirectoryList import *
 import os
 
-class MusicFolder:
-  def __init__(self, path, web_path):
-    self.web_path = web_path
-    self.path = path
+class MusicFolder(DirectoryList):
+  def __init__(self, path, out_path):
+    self.count = 0
+    super(MusicFolder, self).__init__(path)
+    if out_path.endswith('/'):
+      self.out_path = out_path
+    else:
+      self.out_path = "%s%s" % (out_path, '/')
 
-    #clean up file paths
-    if web_path[len(web_path) - 1] != '/':
-      self.web_path += '/'
-    
-    if path[len(path) - 1] != '/':
-      self.path += '/'
-    self.name = path.split('/').pop()
-    self.children = []
-    self.files = {'music': [], 'images': []}
-    self._get_files()
+  def checkFile(self, file_name, path):
+    if file_name.endswith(('.mp3', '.ogg', '.flac', '.png', '.jpg', '.gif')):
+      out_path = path.replace(self.path, self.out_path)
+      base, extension = os.path.splitext(file_name)
 
-  def _get_files(self):
-    items = os.listdir(self.path)
-    children = []
-    for x in items:
-      if os.path.isdir(self.path + x):
-        children.append(x)
+      if not os.path.isfile("%s%s.mp3" % (out_path, base)):
+        return True
+    return False
+
+  def addFolder(self, folder):
+    path = folder["path"]
+    folder["out_path"] = path.replace(self.path, self.out_path)
+
+  def addFile(self, folder, filename, file_path):
+    if filename.endswith(('.mp3', '.ogg', '.flac')):
+      if folder.has_key('audio'):
+        folder["audio"].append(filename)
       else:
-        if not self._add_file(x.replace(self.path, self.web_path), ['.mp3'], 'music'):
-          self._add_file(x.replace(self.path, self.web_path), ['.png', '.jpg', '.gif'], 'images')
-    #add children to our list
-    for c in children:
-      print 'Children %s' % self.path + c
-      self._add_child(MusicFolder(self.path + c, self.web_path + c))
+        folder["audio"] = [filename]
 
-  def _add_file(self, file_name, types, file_type):
-    ret = False;
-    for t in types:
-      try:
-        file_name.index(t)
-        self.files[file_type].append(self.web_path  + file_name)
-        ret = True
-      except ValueError:
-        pass
-    return ret
+    else:
+      if filename.endswith(('.gif', '.png', '.jpg')):
+        if folder.has_key('images'):
+          folder["images"].append(filename)
+        else:
+          folder["images"] = [filename]
 
-  def _add_child(self, child):
-    self.children.append(child)
 
-    if len(child.files['images']):
-      self.files['images'].append(child.files['images'][0])
+if __name__ == "__main__":
+  import sys
+  dl = MusicFolder(sys.argv[1], sys.argv[2])
+  dirs = dl.load()
+  print dirs
+  print "Found %d files" % dl.fileCount
+  print "Found %d folders" % len(dirs)
